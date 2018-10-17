9dd85482468e5b1349fe20f01e48145756f1dc22
pazakin <pazakin@users.noreply.github.com>
2018-10-17 22:15:15 +0200

    Initial commit

---
+++ firstprogram/Main.java
@@ -0,0 +1,12 @@
+package pl.pazakin.firstprogram;
+// to jest tekst
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Czesc jestem Pazakin");
+        /*
+        To jest komentarz na kilka linii
+        na 2
+        na wiele
+        */
+    }
+}
---
+++ variables/Variables.java
@@ -0,0 +1,28 @@
+package pl.pazakin.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 127;
+        byte b1 = -128;
+        short s = Short.MAX_VALUE;
+        int i = Integer.MAX_VALUE;
+        Integer integer = 2;
+        int su = i + 1;
+        long l = Long.MAX_VALUE;
+
+        float f = 0.0f;
+
+        char c = 'f';
+        String str = "Hello Java";
+
+        boolean boo = true;
+        boolean bo = false;
+
+        System.out.println(l);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(su);
+        System.out.println(boo);
+    }
+}
